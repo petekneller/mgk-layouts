950939b3bf592e2616c172b1b24f044282baf81c
Pete Kneller <mrkneller@hotmail.com>
2019-07-25 19:38:14 +0100

    Adds dynamic sizing of the course
    Using a very noddy heuristic. The furthest an obstacle's boundary circle
    can be from the origin of the viewbox is its origin + boundary circle radius
    + boundary circle offset (if applicable). For each obstacle, in each
    axis, find the maximum distance from the origin and set viewbox x =
    viewbox y = that number.

--- src/page-renderer/page-renderer.js
+++ src/page-renderer/page-renderer.js
@@ -5,13 +5,7 @@ const Victor = require('victor');
 const obstacleCtr = require('../obstacle.js');
 const pathCalculator = require('../path-calculator/path-calculator.js');
 
-const svgViewSize = { x: 40, y: 40};
-
-const globalToViewbox = function(x, y) {
-  return { x: x, y: (svgViewSize.y - y) };
-};
-
-const renderObstacle = function(obstacle) {
+const renderObstacle = function(obstacle, globalToViewbox) {
   const {x, y} = globalToViewbox(obstacle.origin.x, obstacle.origin.y);
   switch (obstacle.name) {
   case "StartBox": return `<use href="#start-finish-box" x="${x}" y="${y}" />`;
@@ -30,38 +24,38 @@ const renderObstacle = function(obstacle) {
   }
 };
 
-const renderBoundaryCircle = function(obstacle) {
+const renderBoundaryCircle = function(obstacle, globalToViewbox) {
   const {x, y} = globalToViewbox(obstacle.origin.x, obstacle.origin.y);
   return `<circle r="${obstacle.radius}" cx="${x}" cy="${y}" class="boundaryCircle" />`;
 };
 
-const renderBoundary = function(obstacle) {
+const renderBoundary = function(obstacle, globalToViewbox) {
   let boundaries = '';
 
   if (obstacle.entry === obstacleCtr.EITHER) {
     boundaries = boundaries.concat(renderBoundaryCircle({
       origin: pathCalculator.obstacleLocalVectorToGlobal(obstacle, obstacle.leftEntryBoundaryOrigin),
       radius: obstacle.radius
-    }));
+    }, globalToViewbox));
     boundaries = boundaries.concat(renderBoundaryCircle({
       origin: pathCalculator.obstacleLocalVectorToGlobal(obstacle, obstacle.rightEntryBoundaryOrigin),
       radius: obstacle.radius
-    }));
+    }, globalToViewbox));
   } else {
-    boundaries = boundaries.concat(renderBoundaryCircle(obstacle));
+    boundaries = boundaries.concat(renderBoundaryCircle(obstacle, globalToViewbox));
   }
 
   if (obstacle.exit === obstacleCtr.EITHER) {
     boundaries = boundaries.concat(renderBoundaryCircle({
       origin: pathCalculator.obstacleLocalVectorToGlobal(obstacle, obstacle.leftExitBoundaryOrigin),
       radius: obstacle.radius
-    }));
+    }, globalToViewbox));
     boundaries = boundaries.concat(renderBoundaryCircle({
       origin: pathCalculator.obstacleLocalVectorToGlobal(obstacle, obstacle.rightExitBoundaryOrigin),
       radius: obstacle.radius
-    }));
+    }, globalToViewbox));
   } else {
-    boundaries = boundaries.concat(renderBoundaryCircle(obstacle));
+    boundaries = boundaries.concat(renderBoundaryCircle(obstacle, globalToViewbox));
   }
 
   return boundaries;
@@ -69,7 +63,7 @@ const renderBoundary = function(obstacle) {
   //return renderBoundaryCircle(segment.boundaryCircle1).concat(renderBoundaryCircle(segment.boundaryCircle2));
 };
 
-const renderSegment = function(segment) {
+const renderSegment = function(segment, globalToViewbox) {
   const {x: x1, y: y1} = globalToViewbox(segment.obstacle1.origin.x, segment.obstacle1.origin.y);
   const {x: x2, y: y2} = globalToViewbox(segment.obstacle2.origin.x, segment.obstacle2.origin.y);
   const globalNormal1 = segment.boundaryCircle1.origin.clone().add(segment.exit);
@@ -80,35 +74,63 @@ const renderSegment = function(segment) {
     `<line x1="${xn1}" y1="${yn1}" x2="${xn2}" y2="${yn2}" stroke="black" stroke-width="0.25%" />`;
 };
 
-const renderOrientationVector = function(obstacle) {
+const renderOrientationVector = function(obstacle, globalToViewbox) {
   const orientation = pathCalculator.obstacleLocalVectorToGlobal(obstacle, Victor(0, 1));
   const {x: x1b, y: y1b} = globalToViewbox(obstacle.origin.x, obstacle.origin.y);
   const {x: x2b, y: y2b} = globalToViewbox(orientation.x, orientation.y);
   return `<line x1="${x1b}" y1="${y1b}" x2="${x2b}" y2="${y2b}" stroke="red" stroke-width="0.25%" />`;
 };
 
-const renderNormalVector = function(boundaryCircle, localNormal) {
+const renderNormalVector = function(boundaryCircle, localNormal, globalToViewbox) {
   const {x: x1, y: y1} = globalToViewbox(boundaryCircle.origin.x, boundaryCircle.origin.y);
   const globalNormal = boundaryCircle.origin.clone().add(localNormal);
   const {x: x2, y: y2} = globalToViewbox(globalNormal.x, globalNormal.y);
   return `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" stroke="green" stroke-width="0.25%" />`;
 };
 
+const courseMaxExtents = function(course) {
+  const obstacleExtents = course.map(obstacle => {
+    // a basic heuristic: the boundary can't be offset by more than the largest of the
+    // x or y of the offset. Assumes symmetrical offsets.
+    const maxBoundaryOffset = obstacle.leftBoundaryCircleOffset ?
+          Math.max(obstacle.leftBoundaryCircleOffset.x, obstacle.leftBoundaryCircleOffset.y) : 0;
+
+    return {
+      x: obstacle.origin.x + obstacle.radius + maxBoundaryOffset,
+      y: obstacle.origin.y + obstacle.radius + maxBoundaryOffset
+    };
+  });
+
+  return {
+    x: _.max(obstacleExtents.map(o => o.x)),
+    y: _.max(obstacleExtents.map(o => o.y))
+  };
+};
+
 nunjucks.configure({});
 
 const pageRenderer = function(course) {
+  const { x: maxX, y: maxY } = courseMaxExtents(course);
+  const maxViewbox = Math.floor(Math.max(maxX, maxY) * 1.1);
+  const viewboxExtents = { x: maxViewbox, y: maxViewbox};
+
+  const globalToViewbox = function(x, y) {
+    return { x: x, y: (viewboxExtents.y - y) };
+  };
+
   //_.initial is necessary because _.zip will keep the last pair where the end segment is undefined
   const courseSegments = _.initial(_.zip(course, _.drop(course))).
         map(([o1, o2]) => pathCalculator.calculateSegment(o1, o2));
 
   return nunjucks.render(`${__dirname}/page.njk`, {
+    viewboxExtents,
     courseSegments,
-    renderedObstacles: course.map(renderObstacle),
-    renderedObstacleBoundaries: course.map(renderBoundary),
-    renderedCourseSegments: courseSegments.map(renderSegment),
-    renderedOrientationVectors: course.map(renderOrientationVector),
-    renderedExitVectors: courseSegments.map(segment => renderNormalVector(segment.boundaryCircle1, segment.exit)),
-    renderedEntryVectors: courseSegments.map(segment => renderNormalVector(segment.boundaryCircle2, segment.entry))
+    renderedObstacles: course.map(obstacle => renderObstacle(obstacle, globalToViewbox)),
+    renderedObstacleBoundaries: course.map(obstacle => renderBoundary(obstacle, globalToViewbox)),
+    renderedCourseSegments: courseSegments.map(segment => renderSegment(segment, globalToViewbox)),
+    renderedOrientationVectors: course.map(obstacle => renderOrientationVector(obstacle, globalToViewbox)),
+    renderedExitVectors: courseSegments.map(segment => renderNormalVector(segment.boundaryCircle1, segment.exit, globalToViewbox)),
+    renderedEntryVectors: courseSegments.map(segment => renderNormalVector(segment.boundaryCircle2, segment.entry, globalToViewbox))
   });
 };
 
