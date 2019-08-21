7f0da2cd5bf1899f4fac12f79c687053db3f823c
Pete Kneller <mrkneller@hotmail.com>
2019-08-21 17:49:46 +0100

    First prototype of the SVG grid in React
    Rough flexbox layout to define the course grid and (what will probably
    be a) properties box on the right hand side. Rough prototype of a grid
    that has minor/major lines, expands to fill the viewport and has major
    tick marks in the left and bottom margin.
    
    Next: prototype zooming and panning of this grid.

--- src/App.js
+++ src/App.js
@@ -14,14 +14,25 @@ function App() {
           gridTemplateRows: 'auto 30px'
         }}>
           <div className='grid-item' style={{ gridColumn: '1', gridRow: '1' }}>
-            <h1>1</h1>
+            <div style={{ position:'relative', height:'100%', width:'100%' }} >
+              <h1 style={{ position: 'absolute' }}>1</h1>
+            </div>
           </div>
           <div className='grid-item' style={{ gridColumn: '2', gridRow: '2' }}></div>
-          <div className='grid-item' style={{ gridColumn: '2', gridRow: '1',
+          <div className='grid-item' style={{ gridColumn: '2', gridRow: '1', position: 'relative',
                                              borderLeft: '2px solid black',
                                              borderBottom: '2px solid black' }}>
-            <svg style={{ width: '100%', height: '100%', overflow: 'hidden' }} viewbox='0 0 100 100'>
-      { [...Array(10).keys()].map(idx => <line y1=0 y2=100 x1={idx * 10} x2={idx * 10} stroke='black'/>) }
+            <svg viewBox='0 0 100 100' preserveAspectRatio='xMinYMax slice' style={{ width:'100%', height:'100%', overflow:'hidden', position:'absolute', bottom:'0' }} >
+              { [...Array(100).keys()].map(idx => <line y1='0' y2='100' x1={idx} x2={idx} style={{ stroke:'lightgray', strokeWidth:'0.2%' }} />) }
+              { [...Array(100).keys()].map(idx => <line x1='0' x2='100' y1={idx} y2={idx} style={{ stroke:'lightgray', strokeWidth:'0.2%' }} />) }
+              { [...Array(10).keys()].map(idx => <line y1='0' y2='100' x1={idx * 10} x2={idx * 10} style={{ stroke:'darkgray', strokeWidth:'0.2%' }} />) }
+              { [...Array(10).keys()].map(idx => <line x1='0' x2='100' y1={idx * 10} y2={idx * 10} style={{ stroke:'darkgray', strokeWidth:'0.2%' }} />) }
+            </svg>
+            <svg viewBox='0 0 100 100' preserveAspectRatio='xMinYMax slice' style={{ width:'100%', height:'100%', overflow:'hidden', position:'absolute', bottom:'0', left:'-30px' }} >
+              { [...Array(9).keys()].map(idx => <line x1='0' x2='2' y1={idx * 10 + 10} y2={idx * 10 + 10} style={{ stroke:'black', strokeWidth:'0.2%' }} />) }
+            </svg>
+            <svg viewBox='0 0 100 100' preserveAspectRatio='xMinYMax slice' style={{ width:'100%', height:'100%', overflow:'visible', position:'absolute', bottom:'-30px' }} >
+              { [...Array(9).keys()].map(idx => <line y1='100' y2='98' x1={idx * 10 + 10} x2={idx * 10 + 10} style={{ stroke:'black', strokeWidth:'0.2%' }} />) }
             </svg>
          </div>
         </div>
