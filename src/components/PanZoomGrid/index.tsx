4977d399d7481e77039d4c1820c297c9c81c2a75
Pete Kneller <mrkneller@hotmail.com>
2019-08-22 16:57:08 +0100

    Disambiguates the extents of the _course_ from the other extents

--- src/components/PanZoomGrid/index.tsx
+++ src/components/PanZoomGrid/index.tsx
@@ -3,7 +3,7 @@ import styles from './PanZoomGrid.module.css';
 import GridViewport from './GridViewport';
 
 const maxViewboxExtent = 100;
-const maxExtent = 1000;
+const maxCourseExtent = 1000;
 
 const viewboxCentre = maxViewboxExtent / 2;
 
@@ -45,17 +45,17 @@ const PanZoomGrid: React.FC = () => {
 
             <GridViewport positioningStyle='viewport-main' maxExtent={maxViewboxExtent}>
               <g transform={ zoomPanTransform(panCentre, zoomFactor) }>
-                { [...Array(maxExtent).keys()].map(idx => <line y1='0' y2={maxExtent} x1={idx} x2={idx} style={{ stroke:'lightgray', strokeWidth:'0.2%' }} />) }
-                { [...Array(maxExtent).keys()].map(idx => <line x1='0' x2={maxExtent} y1={idx} y2={idx} style={{ stroke:'lightgray', strokeWidth:'0.2%' }} />) }
-                { [...Array(maxExtent/10).keys()].map(idx => <line y1='0' y2={maxExtent} x1={idx * 10} x2={idx * 10} style={{ stroke:'darkgray', strokeWidth:'0.2%' }} />) }
-                { [...Array(maxExtent/10).keys()].map(idx => <line x1='0' x2={maxExtent} y1={idx * 10} y2={idx * 10} style={{ stroke:'darkgray', strokeWidth:'0.2%' }} />) }
+                { [...Array(maxCourseExtent).keys()].map(idx => <line y1='0' y2={maxCourseExtent} x1={idx} x2={idx} style={{ stroke:'lightgray', strokeWidth:'0.2%' }} />) }
+                { [...Array(maxCourseExtent).keys()].map(idx => <line x1='0' x2={maxCourseExtent} y1={idx} y2={idx} style={{ stroke:'lightgray', strokeWidth:'0.2%' }} />) }
+                { [...Array(maxCourseExtent/10).keys()].map(idx => <line y1='0' y2={maxCourseExtent} x1={idx * 10} x2={idx * 10} style={{ stroke:'darkgray', strokeWidth:'0.2%' }} />) }
+                { [...Array(maxCourseExtent/10).keys()].map(idx => <line x1='0' x2={maxCourseExtent} y1={idx * 10} y2={idx * 10} style={{ stroke:'darkgray', strokeWidth:'0.2%' }} />) }
                 <circle cx='50' cy='50' r='5' stroke='red' fill='blue'/>
               </g>
             </GridViewport>
 
             <GridViewport positioningStyle='viewport-left-axis' maxExtent={maxViewboxExtent}>
               <g transform={ panYTransform(panCentre.y, zoomFactor) }>
-                { [...Array(maxExtent/10 - 1).keys()].map(idx => {
+                { [...Array(maxCourseExtent/10 - 1).keys()].map(idx => {
                   const i = idx + 1;
                   const y = i * 10;
                   const scaledY = y * zoomFactor;
@@ -66,7 +66,7 @@ const PanZoomGrid: React.FC = () => {
 
             <GridViewport positioningStyle='viewport-bottom-axis' maxExtent={maxViewboxExtent}>
               <g transform={ panXTransform(panCentre.x, zoomFactor) }>
-                { [...Array(maxExtent/10 - 1).keys()].map(idx => {
+                { [...Array(maxCourseExtent/10 - 1).keys()].map(idx => {
                   const i = idx + 1;
                   const x = i * 10;
                   const scaledX = x * zoomFactor;
