308726121483b5bcf13cb1c106fbf9b56b77f65c
Pete Kneller <mrkneller@hotmail.com>
2020-07-25 16:24:52 +0100

    Begins the process of re-styling the top....
    by adding a title banner.

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,22 @@ import PanZoomGrid from './components/PanZoomGrid';
 const App: React.FC = () => {
   // { box-sizing: border-box } changes the box model to include padding in the 100% sizing
   return (
-      <div className={ styles['outer-spacing'] }>
+    <div id={ styles['root-container'] }>
+      <header className={ styles['outer-margin'] }>
+        <div id={ styles['title-block'] }>
+          <h1>Motogymkhana</h1>
+          <h2>Course Designer</h2>
+        </div>
+      </header>
+
+      <div id={ styles['content-separator'] }></div>
+
+      <div id={ styles['body-container'] } className={ styles['outer-margin'] }>
         <SlideOut>
           <PanZoomGrid></PanZoomGrid>
         </SlideOut>
       </div>
+    </div>
   );
 }
 
